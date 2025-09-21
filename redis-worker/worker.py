ffc201ddddcdb9f623d15d8709bbd49cef28e99e
Jacob Poling <polingj999@gmail.com>
2025-09-21 19:38:27 +0000

    Added postgres and redis and worker. Needs work, like reading from env. Also maybe I wanna use Kafka

--- redis-worker/worker.py
+++ redis-worker/worker.py
@@ -2,7 +2,7 @@ import os
 import redis
 
 from dotenv import load_dotenv
-from rq import Worker, Queue, Connection
+from rq import Worker, Queue
 
 # read in env file
 basedir = os.path.abspath(os.path.dirname(__name__))
@@ -10,10 +10,9 @@ dotenv_path = os.path.join(basedir, '.env')
 load_dotenv(dotenv_path)
 
 listen = ['default']
-port = os.getenv('REDIS_PORT', 6380)
-conn = redis.from_url(f'redis://127.0.0.1:{port}')
+port = os.getenv('REDIS_PORT', 6379)
+conn = redis.from_url(f'redis://redis:{port}')
 
 if __name__ == '__main__':
-    with Connection(conn):
-        worker = Worker(list(map(Queue, listen)))
-        worker.work()
+    worker = Worker(['default'], connection=conn)
+    worker.work()
